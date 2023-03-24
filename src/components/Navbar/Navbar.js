abb9f64366eba2347a18966fb49ebc1a71870ff5
David Isidore <isidoredavid@protonmail.com>
2023-03-24 16:51:31 +0100

    Implémentation de la navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,6 +12,24 @@ export default function Navbar() {
                 <li>
                     <Link to="/about">Qui sommes-nous / La brasserie</Link>
                 </li>
+                <li>
+                    <Link to="/beer">Notre gamme de bières / Nos services</Link>
+                </li>
+                <li>
+                    <Link to="/news">Actualités</Link>
+                </li>
+                <li>
+                    <Link to="/visits">Les visites</Link>
+                </li>
+                <li>
+                    <Link to="/workshop">Nos ateliers brassage</Link>
+                </li>
+                <li>
+                    <Link to="/gallery">Galerie de photos</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
             </ul>
         </div>
     )
