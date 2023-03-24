87961d090ed82d5c339e94e241811a37bb3fa68e
David Isidore <isidoredavid@protonmail.com>
2023-03-24 16:21:30 +0100

    Implémentation de la page About

---
+++ src/Pages/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
+
+export default function About() {
+    return (
+        <main>
+            <Header />
+            <div></div>
+            <Footer />
+        </main>
+    )
+}
