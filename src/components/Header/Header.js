6d05961d068edc060003f6b31fdecdc1d9a63e8c
David Isidore <isidoredavid@protonmail.com>
2023-03-24 15:26:12 +0100

    Modification du header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
-import React from "react";
-import logo from '../../assets/logo.png'
+import React from 'react'
+import logoNB from '../../assets/logoNB.png'
+import ab from '../../assets/logoAB.png'
+import './header.scss'
 
-export default function Header(){
-    return(
-        <div>
-            <img src={logo} alt="logo" />
-            <div>
-                <ul>
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="header_nav">
+                <ul className="header_nav_list">
                     <li>Accueil</li>
                     <li>Qui sommes-nous / La brasserie</li>
                     <li>Notre gamme de bières / Nos services</li>
@@ -15,9 +16,11 @@ export default function Header(){
                     <li>Ateliers brassage</li>
                     <li>Galerie photos</li>
                     <li>Contact / Horaires</li>
-
                 </ul>
             </div>
+
+            <img src={logoNB} alt="logo" className="header_img" />
+            <img src={ab} alt="logo AB" className="ab" />
         </div>
     )
 }
